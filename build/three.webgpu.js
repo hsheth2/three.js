48903b3eb70a0f8d0673a27fe0ec15934590ac3b
hsheth2 <hsheth2@gmail.com>
2026-06-09 20:05:34 +0000

    Treehouse clustered lighting fixes

--- build/three.webgpu.js
+++ build/three.webgpu.js
@@ -45055,6 +45055,25 @@ class ShadowNode extends ShadowBaseNode {
 	 */
 	updateShadow( frame ) {
 
+		const ownerInstanceId = this.light && this.light.userData ? this.light.userData.approximateLightOwnerInstanceId : undefined;
+		const ownerMuted = [];
+		if ( typeof ownerInstanceId === 'string' && ownerInstanceId.length > 0 ) {
+
+			frame.scene.traverse( ( obj ) => {
+
+				if ( obj.isMesh === true && obj.castShadow === true && obj.userData && obj.userData.approximateLightShadowOwnerInstanceId === ownerInstanceId ) {
+
+					obj.castShadow = false;
+					ownerMuted.push( obj );
+
+				}
+
+			} );
+
+		}
+
+		try {
+
 		const { shadowMap, light, shadow } = this;
 		const { renderer, scene, camera } = frame;
 
@@ -45101,6 +45120,12 @@ class ShadowNode extends ShadowBaseNode {
 		shadow.camera.layers.mask = _shadowCameraLayer;
 
 		restoreRendererAndSceneState( renderer, scene, _rendererState );
+
+		} finally {
+
+			for ( const mesh of ownerMuted ) mesh.castShadow = true;
+
+		}
 
 	}
 
--- src/nodes/lighting/ShadowNode.js
+++ src/nodes/lighting/ShadowNode.js
@@ -720,6 +720,25 @@ class ShadowNode extends ShadowBaseNode {
 	 */
 	updateShadow( frame ) {
 
+		const ownerInstanceId = this.light && this.light.userData ? this.light.userData.approximateLightOwnerInstanceId : undefined;
+		const ownerMuted = [];
+		if ( typeof ownerInstanceId === 'string' && ownerInstanceId.length > 0 ) {
+
+			frame.scene.traverse( ( obj ) => {
+
+				if ( obj.isMesh === true && obj.castShadow === true && obj.userData && obj.userData.approximateLightShadowOwnerInstanceId === ownerInstanceId ) {
+
+					obj.castShadow = false;
+					ownerMuted.push( obj );
+
+				}
+
+			} );
+
+		}
+
+		try {
+
 		const { shadowMap, light, shadow } = this;
 		const { renderer, scene, camera } = frame;
 
@@ -766,6 +785,12 @@ class ShadowNode extends ShadowBaseNode {
 		shadow.camera.layers.mask = _shadowCameraLayer;
 
 		restoreRendererAndSceneState( renderer, scene, _rendererState );
+
+		} finally {
+
+			for ( const mesh of ownerMuted ) mesh.castShadow = true;
+
+		}
 
 	}
 
